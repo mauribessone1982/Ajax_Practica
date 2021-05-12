e639a7f3f003f8039599395fb17391d88769bc9a
Mauricio Bessone <mauribessone@gmail.com>
2021-05-11 23:34:05 -0300

    ajax

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
+const { type } = require("jquery");
+
 $('#boton').on('click', function() {
   $('#a-content').load('https://norfipc.com/include/ajax');
   $('#boton').hide();
 })
+
+$(function(){
+   $('#ejecutar').click(function(){
+      sumar($('#valor1').val(),$('#valor2').val());
+               });
+   function sumar(val1,val2){
+      var parametros= {
+         "val1":val1,
+         "val2":val2
+      }
+      $.ajax({
+            data:parametros,
+            url:"suma.php",
+            type:"post",
+            beforeSend:function(){
+               consol.log("se esta procesandotu petición");
+            }
+
+      })
+      .done(function(data){
+         $('#resultado').text(data);
+      });
+   }
+});
